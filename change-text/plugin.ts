384d6f5b941d2114177f0394c020a98bd415117a
Jenil Gogari <jenil27@hotmail.com>
2021-09-26 12:25:02 -0400

    add support for regex

--- change-text/plugin.ts
+++ change-text/plugin.ts
@@ -1,65 +1,65 @@
 var textSelection = filterData(
-  figma.currentPage.selection,
-  i => i.type == "TEXT"
+    figma.currentPage.selection,
+    (i) => i.type == 'TEXT'
 );
 
 figma.showUI(__html__, {
-  width: 600,
-  height: 300
+    width: 600,
+    height: 300,
 });
 
 if (!textSelection.length)
-  figma.closePlugin("Please select a few layers and then run the plugin.");
+    figma.closePlugin('Please select a few layers and then run the plugin.');
 
 figma.ui.postMessage({
-  type: "setSelectionMsg",
-  value: `Selected ${figma.currentPage.selection.length} layers with ${
-    textSelection.length
-  } text layers`,
-  count: textSelection.length,
-  selection: textSelection.map(i => i.characters)
+    type: 'setSelectionMsg',
+    value: `Selected ${figma.currentPage.selection.length} layers with ${textSelection.length} text layers`,
+    count: textSelection.length,
+    selection: textSelection.map((i) => i.characters),
 });
 
-figma.ui.onmessage = async msg => {
-  console.log(msg);
+figma.ui.onmessage = async (msg) => {
+    console.log(msg);
 
-  if (msg.type == "update-text") {
-    console.log(textSelection);
+    if (msg.type == 'update-text') {
+        console.log(textSelection);
 
-    for (const text of textSelection) {
-      await figma.loadFontAsync(text.fontName as FontName);
-      if (text.type === "TEXT" && text.characters) {
-        let exp = msg.match ? msg.match : text.characters;
+        for (const text of textSelection) {
+            await figma.loadFontAsync(text.fontName as FontName);
+            if (text.type === 'TEXT' && text.characters) {
+                let exp = msg.match ? msg.match : text.characters;
 
-        text.characters = text.characters.replace(
-          new RegExp(
-            exp.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&"),
-            msg.ignoreCase ? "ig" : "g"
-          ),
-          msg.value
-        );
-      }
+                text.characters = text.characters.replace(
+                    new RegExp(
+                       msg.isRegex
+                            ? exp
+                            : exp.replace(/[-[\]{}()*+?\.,\\^$|#\s]/g, '\\$&'),
+                        msg.ignoreCase ? 'ig' : 'g'
+                    ),
+                    msg.value
+                );
+            }
+        }
+        figma.closePlugin('🎉');
+    } else {
+        figma.closePlugin();
     }
-    figma.closePlugin("🎉");
-  } else {
-    figma.closePlugin();
-  }
 };
 
 function filterData(data, predicate) {
-  return !!!data
-    ? null
-    : data.reduce((list, entry) => {
-        let clone = null;
-        if (predicate(entry)) {
-          clone = entry;
-          list.push(clone);
-        } else if (entry.children != null) {
-          let children = filterData(entry.children, predicate);
-          if (children.length > 0) {
-            list.push(...children);
-          }
-        }
-        return list;
-      }, []);
+    return !!!data
+        ? null
+        : data.reduce((list, entry) => {
+              let clone = null;
+              if (predicate(entry)) {
+                  clone = entry;
+                  list.push(clone);
+              } else if (entry.children != null) {
+                  let children = filterData(entry.children, predicate);
+                  if (children.length > 0) {
+                      list.push(...children);
+                  }
+              }
+              return list;
+          }, []);
 }
--- change-text/ui.ts
+++ change-text/ui.ts
@@ -1,86 +1,93 @@
-const match = <HTMLInputElement>document.getElementById("matchVal");
-const textbox = <HTMLInputElement>document.getElementById("value");
-const ignoreCase = <HTMLInputElement>document.getElementById("ignoreCase");
+const match = <HTMLInputElement>document.getElementById('matchVal');
+const textbox = <HTMLInputElement>document.getElementById('value');
+const ignoreCase = <HTMLInputElement>document.getElementById('ignoreCase');
+const isRegex = <HTMLInputElement>document.getElementById('isRegex');
 let selection = [];
+
+textbox.focus();
 
 match.oninput = updatePreview;
 textbox.oninput = updatePreview;
 ignoreCase.onchange = updatePreview;
+isRegex.onchange = updatePreview;
 
-document.getElementById("update").onclick = () => {
-  let confirmation = true;
+document.getElementById('update').onclick = () => {
+    let confirmation = true;
 
-  if (!textbox.value && !match.value)
-    confirmation = window.confirm(
-      "This will clear the text in the selected layers. Are you absolutely sure?"
-    );
+    if (!textbox.value && !match.value)
+        confirmation = window.confirm(
+            'This will clear the text in the selected layers. Are you absolutely sure?'
+        );
 
-  if (confirmation) {
-    parent.postMessage(
-      {
-        pluginMessage: {
-          type: "update-text",
-          value: textbox.value,
-          match: match.value,
-          ignoreCase: ignoreCase.checked
-        }
-      },
-      "*"
-    );
-  } else {
-    parent.postMessage(
-      {
-        pluginMessage: {
-          type: "cancel"
-        }
-      },
-      "*"
-    );
-  }
-};
-
-document.getElementById("cancel").onclick = () => {
-  parent.postMessage({ pluginMessage: { type: "cancel" } }, "*");
-};
-
-onmessage = event => {
-  console.log("Selected layers: ", event.data.pluginMessage.count);
-  if (event.data.pluginMessage.type == "setSelectionMsg") {
-    if (event.data.pluginMessage.count > 0) {
-      document.getElementById("count").innerHTML =
-        event.data.pluginMessage.value;
-      document.getElementById("selection").style.display = "block";
+    if (confirmation) {
+        parent.postMessage(
+            {
+                pluginMessage: {
+                    type: 'update-text',
+                    value: textbox.value,
+                    match: match.value,
+                    ignoreCase: ignoreCase.checked,
+                    isRegex: isRegex.checked,
+                },
+            },
+            '*'
+        );
     } else {
-      document.getElementById("count").innerHTML =
-        "Please select a few layers and then run the plugin";
-      document.getElementById("selection").style.display = "none";
+        parent.postMessage(
+            {
+                pluginMessage: {
+                    type: 'cancel',
+                },
+            },
+            '*'
+        );
     }
+};
 
-    console.log(event.data.pluginMessage.selection);
-    selection = event.data.pluginMessage.selection;
-    updatePreview();
-  }
+document.getElementById('cancel').onclick = () => {
+    parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*');
+};
+
+onmessage = (event) => {
+    console.log('Selected layers: ', event.data.pluginMessage.count);
+    if (event.data.pluginMessage.type == 'setSelectionMsg') {
+        if (event.data.pluginMessage.count > 0) {
+            document.getElementById('count').innerHTML =
+                event.data.pluginMessage.value;
+            document.getElementById('selection').style.display = 'block';
+        } else {
+            document.getElementById('count').innerHTML =
+                'Please select a few layers and then run the plugin';
+            document.getElementById('selection').style.display = 'none';
+        }
+
+        console.log(event.data.pluginMessage.selection);
+        selection = event.data.pluginMessage.selection;
+        updatePreview();
+    }
 };
 
 function updatePreview() {
-  let previewHTML = "";
-  const item = document.querySelector("#preview-item").innerHTML;
+    let previewHTML = '';
+    const item = document.querySelector('#preview-item').innerHTML;
 
-  selection.forEach(text => {
-    let newText = "";
-    let exp = match.value ? match.value : text;
+    selection.forEach((text) => {
+        let newText = '';
+        let exp = match.value ? match.value : text;
 
-    newText = text.replace(
-      new RegExp(
-        exp.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&"),
-        ignoreCase.checked ? "ig" : "g"
-      ),
-      textbox.value
-    );
+        newText = text.replace(
+            new RegExp(
+                isRegex.checked
+                    ? exp
+                    : exp.replace(/[-[\]{}()*+?\.,\\^$|#\s]/g, '\\$&'),
+                ignoreCase.checked ? 'ig' : 'g'
+            ),
+            textbox.value
+        );
 
-    previewHTML += item
-      .replace(/{{CURRENT}}/g, text)
-      .replace(/{{NEW}}/g, newText);
-  });
-  document.querySelector(".preview dl").innerHTML = previewHTML;
+        previewHTML += item
+            .replace(/{{CURRENT}}/g, text)
+            .replace(/{{NEW}}/g, newText);
+    });
+    document.querySelector('.preview dl').innerHTML = previewHTML;
 }
